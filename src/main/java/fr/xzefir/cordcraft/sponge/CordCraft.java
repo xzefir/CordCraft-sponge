8173c8a38eac029ba4152ac22c63196bf835cf4d
Xzefir <evan64g@gmail.com>
2021-08-03 14:11:39 +0200

    Bump version to 1.0.1

--- src/main/java/fr/xzefir/cordcraft/sponge/CordCraft.java
+++ src/main/java/fr/xzefir/cordcraft/sponge/CordCraft.java
@@ -36,7 +36,7 @@ public class CordCraft {
     static CordCraft instance = null;
 
     public static final String API_URL = "http://vps.craftmoney.fr:27980/";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final int versionDataFile = 3;
 
     public static String token = "";
