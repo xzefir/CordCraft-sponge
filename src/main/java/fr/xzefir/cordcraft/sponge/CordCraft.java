c58d6154d14df32eef54b03c212c28563f872616
Xzefir <Evan64g@gmail.com>
2022-09-26 08:29:48 +0200

    Fix Minecraft to Discord communication

--- src/main/java/fr/xzefir/cordcraft/sponge/CordCraft.java
+++ src/main/java/fr/xzefir/cordcraft/sponge/CordCraft.java
@@ -35,8 +35,8 @@ public class CordCraft {
 
     static CordCraft instance = null;
 
-    public static final String API_URL = "http://vps.craftmoney.fr:27980/";
-    public static final String VERSION = "1.0.1";
+    public static final String API_URL = "http://v02.owntech.fr:3001";
+    public static final String VERSION = "1.0.2";
     public static final int VERSION_DATA_FILE = 3;
 
     public static String token = "";
